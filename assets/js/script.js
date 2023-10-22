afa5636ddddedd9ab13f4a6a80fb3ecd0a8e9cfa
nariman tatari <nariman.tatari@mail.com>
2023-10-22 11:16:37 +0330

    fish the desktop mode styles and javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,17 @@ navToggleBtn=()=>{
 }
 navMenuBtn.addEventListener('click',navToggleBtn);
 navCloseBtn.addEventListener('click',navToggleBtn);
+
+
+const themebtn=document.querySelectorAll('.theme-btn');
+
+for(let i=0;i<themebtn.length;i++){
+    themebtn[i].addEventListener('click',()=>{
+        document.body.classList.toggle('light-theme');
+        document.body.classList.toggle('dark-theme');
+        for(let j=0;j<themebtn.length;j++){
+            themebtn[j].classList.toggle('light');
+            themebtn[j].classList.toggle('dark');
+        }
+    });
+}
